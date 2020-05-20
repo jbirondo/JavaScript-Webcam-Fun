8041a8b9d970e65142ba7f9755850c0cade56f58
Eugene Birondo <jbirondo@gmail.com>
2020-05-19 20:41:55 -0700

    Working on takePhoto

--- scripts.js
+++ scripts.js
@@ -30,7 +30,11 @@ function takePhoto() {
     snap.currenTime = 0
     snap.play()
     const data = canvas.toDataURL('image/jpeg')
-    console.log(data)
+    const link = document.createElement('a')
+    link.href = data
+    link.setAttribute('download', 'handsome')
+    link.textContent = 'Download Image'
+    strip.insertBefore(link, strip.firstChild)
 }
 
 getVideo()
