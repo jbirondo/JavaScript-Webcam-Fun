531efc7b8719a5d4841f77867793f9b57943e31c
Eugene Birondo <jbirondo@gmail.com>
2020-05-20 22:46:36 -0700

    Working on greenScreen

--- scripts.js
+++ scripts.js
@@ -69,7 +69,14 @@ function greenScreen(pixels){
         let blue = pixels.data[i + 2]
         let alpha = pixels.data[i + 3];
 
-        
+        if(red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax){
+                pixels.data[i + 3] = 0
+            }
     }
 }
 
